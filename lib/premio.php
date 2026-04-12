ddcde11a22d1275b785b92893639225ef092547d
Federico Corvalan <federico.corvalan@sancorsalud.com.ar>
2026-04-12 15:30:00 -0300

    feat: calculo de premio por modalidad cabeza y numero

---
+++ lib/premio.php
@@ -0,0 +1,40 @@
+<?php
+// Calculo de premio de una apuesta contra las extracciones de su sorteo.
+// NOTA: complejidad intencional, sin extraer funciones auxiliares - modulo
+// candidato a intervencion preventiva (ver auditoria del curso, E7/E8).
+
+function calcularPremio($apuesta, $extracciones) {
+    $numero = $apuesta['numero_apostado'];
+    $modalidad = $apuesta['modalidad'];
+    $monto = (float)$apuesta['monto'];
+
+    if ($modalidad === 'cabeza') {
+        foreach ($extracciones as $ext) {
+            if ((int)$ext['posicion'] === 1) {
+                if ($ext['numero'] !== null) {
+                    if ($numero === $ext['numero']) {
+                        return $monto * 70;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    return 0;
+                }
+            }
+        }
+        return 0;
+    } else {
+        if ($modalidad === 'numero') {
+            foreach ($extracciones as $ext) {
+                if ($ext['numero'] !== null) {
+                    if ($numero === $ext['numero']) {
+                        return $monto * 7;
+                    }
+                }
+            }
+            return 0;
+        } else {
+            return 0;
+        }
+    }
+}
